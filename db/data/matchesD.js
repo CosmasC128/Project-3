e1df852a8bb043f75bcdab5b42c8b5ad0e535ba3
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-10-01 14:57:15 +0100

    seeded database

--- db/data/matchesD.js
+++ db/data/matchesD.js
@@ -4,5 +4,59 @@ export default [
     url: 'https://www.youtube.com/embed/4clJTZCPHbQ',
     views: 1,
     votes: 1
-  }
+  },
+  {
+    title: 'LNG vs WE - Game 3 | Regional Qualifier LPL Summer 2021 | LNG Gaming vs Team WE G3',
+    url: 'https://www.youtube.com/embed/i9rJTOuFGUg',
+    views: 1,
+    votes: 1
+  },
+  {
+    title: 'LNG vs WE - Game 2 | Regional Qualifier LPL Summer 2021 | LNG Gaming vs Team WE G2',
+    url: 'https://www.youtube.com/embed/Y4mdL6FLGzI',
+    views: 1,
+    votes: 1
+  },
+  {
+    title: 'LNG vs WE - Game 1 | Regional Qualifier LPL Summer 2021 | LNG Gaming vs Team WE G1',
+    url: 'https://www.youtube.com/embed/i8AlYWZMYUg',
+    views: 1,
+    votes: 1
+  },
+  {
+    title: 'RA vs LNG - Game 3 | Regional Qualifier LPL Summer 2021 | Rare Atom vs LNG Gaming G3',
+    url: 'https://www.youtube.com/embed/v5uZR_s-QkE',
+    views: 1,
+    votes: 1
+  },
+  {
+    title: 'RA vs LNG - Game 2 | Regional Qualifier LPL Summer 2021 | Rare Atom vs LNG Gaming G2',
+    url: 'https://www.youtube.com/embed/NnrysKdznX0',
+    views: 1,
+    votes: 1
+  },
+  {
+    title: 'RA vs LNG - Game 1 | Regional Qualifier LPL Summer 2021 | Rare Atom vs LNG Gaming G1',
+    url: 'https://www.youtube.com/embed/3w9NgRhsKNw',
+    views: 1,
+    votes: 1
+  },
+  {
+    title: 'WE vs RNG - Game 3 | Regional Qualifier LPL Summer 2021 | Team WE vs Royal Never Give Up G3',
+    url: 'https://www.youtube.com/embed/NhebWJhVQe4',
+    views: 1,
+    votes: 1
+  },
+  {
+    title: 'WE vs RNG - Game 2 | Regional Qualifier LPL Summer 2021 | Team WE vs Royal Never Give Up G2',
+    url: 'https://www.youtube.com/embed/i7CjUxwQhns',
+    views: 1,
+    votes: 1
+  },
+  {
+    title: 'WE vs RNG - Game 1 | Regional Qualifier LPL Summer 2021 | Team WE vs Royal Never Give Up G1',
+    url: 'https://www.youtube.com/embed/SbmPf-26mRA',
+    views: 1,
+    votes: 1
+  },
 ]
