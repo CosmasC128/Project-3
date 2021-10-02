be11c9423025785751eb75095a1dadfce10c440f
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-10-02 15:37:54 +0100

    code merged the old fashioned way

--- client/src/components/Match.js
+++ client/src/components/Match.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+import { getTokenFromLocalStorage } from './helpers/auth.js'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const Match = ({ matchesArray }) => {
   
@@ -13,16 +15,17 @@ const Match = ({ matchesArray }) => {
     }
   }
   
+  const title = match.title
   const url = match.url
   const rating = match.rating
   const views = match.views
   const comments = match.comments
+  let votes = match.votes // replace with database get later
 
-  //need to update views on visit, then save to database
 
   //Button functionality
   //set the votes equal to the database vote total
-  let votes = match.votes
+  //need to update views on visit, then save to database
   //handle the click event and for the fire button
   const [clicked, setClicked] = useState(false)
   function handleClick(e) {
@@ -38,17 +41,60 @@ const Match = ({ matchesArray }) => {
     }  
   }
 
+  const [formData, setFormData] = useState({
+    text: '',
+    rating: '',
+    owner: '',
+
+  })
+
+  const handleChange = (event) => {
+    const newComment = { ...formData, [event.target.name]: event.target.value }
+    setFormData(newComment)
+  }
+  // console.log('form daaaa ==>', formData)
+
+  const handleSubmit = async () => {
+    try {
+      await axios.post(
+        `/api/matches/${id}/comments`,
+        formData,
+        {
+          headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+        }
+      )
+    } catch (err) {
+      console.log(err)  
+    }
+  }
+  // console.log('comments ===>', comments)
+
   return (<>
-    <div>Watch your favourite match: {id}</div>
-    <iframe width="560" height="315" src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    <div>Fire Rating: {rating}</div>
-    <div>Views: {views}</div>
+    <div className="playerWrapper">
+      <div>{ title }</div>
+      <iframe width="560" height="315" src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <div className="fireWrap">
+        <div>Fire Rating: {rating}</div>
+        <div className='fireBtn'><button className="btn btn-primary" type="submit" onClick={handleClick}>🔥 Fire 🔥</button></div>
+      </div>
+      <div>Views: {views}</div>
+    </div>
     { comments ? comments.map(comment => { 
-      return <div key={comment._id}>{comment.text} comment found </div> 
+      return <div key={comment._id}>{comment.text}</div> 
     })
       :
       <div>No comments yet</div> }
-    <div className='fireBtn'><button className="btn btn-primary" type="submit" onClick={handleClick}>🔥 Fire 🔥</button></div>
+    <form onSubmit={handleSubmit}>
+      <textarea
+        type="text" 
+        placeholder="Write a comment... " 
+        name="text" 
+        onChange={handleChange}
+        value={formData.text}
+      >
+      </textarea>
+      <button>Submit</button>
+    </form>
   </>)
 }
 export default Match
