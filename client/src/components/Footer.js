2b7d05ea7784a64eb2af384760d4c8884303dd48
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-10-07 14:39:20 +0100

    top five on homepage

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
       <footer id="footer">
         <div className="col-md-4 d-flex align-items-center">
           <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-            <svg className="bi" width="30" height="24"><use xlinkhref="#bootstrap"/></svg>
+            <svg className="bi" width="30" height="24"><use xlinkHref="#bootstrap"/></svg>
           </a>
           <div className="leftText">
             <span>&copy; 2021</span>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Home = () => {
+
+  const [ matches, setMatches ] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('/api/matches')
+        setMatches(Object.values({ ...data }))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+
+  let topFive = []
+  if (matches){
+    topFive = (matches.sort((a,b)=> (a['rating'] < b['rating'] ? 1 : -1)).slice(0,5))
+  }
 
   return (
     <div className='bg' style={{
@@ -10,30 +31,39 @@ const Home = () => {
         <h1 className='homeTitle'> 🔥 GCW Fire Esports 🔥</h1>
         <h2 className="homeFlavour">Catch up with the weeks hottest E-Sport Action</h2>
       </div>
-      <section id="homeSection">
-        <div className="topGames" >
-          <div className="hl-games-list-item">
-            <div className="img-bg">
-              <img className="homeImages" src="https://smallcaps.com.au/wp-content/uploads/2019/03/Mogul-ASX-ESH-Legacy-AFL-Adelaide-Crows-eSports-team.jpg" alt="Game"></img>
+      { matches.length ?
+        <section id="homeSection">
+          <div className="topGames" >
+            <div className="hl-games-list-item">
+              <Link to={`/matches/${ topFive[0]._id }`}>
+                <img className="homeImages" src={ topFive[0].thumbNail } alt="Game"></img>
+              </Link>  
+            </div>
+            <div className="hl-games-list-item">
+              <Link to={`/matches/${ topFive[1]._id }`}>
+                <img className="homeImages" src={ topFive[1].thumbNail } alt="Game"></img>
+              </Link>
+            </div>
+            <div className="hl-games-list-item">
+              <Link to={`/matches/${ topFive[2]._id }`}>
+                <img className="homeImages" src={ topFive[2].thumbNail } alt="Game"></img>
+              </Link>
+            </div>
+            <div className="hl-games-list-item">
+              <Link to={`/matches/${ topFive[3]._id }`}>
+                <img className="homeImages" src={ topFive[3].thumbNail } alt="Game"></img>
+              </Link>
+            </div>
+            <div className="hl-games-list-item">
+              <Link to={`/matches/${ topFive[4]._id }`}>
+                <img className="homeImages" src={ topFive[4].thumbNail } alt="Game"></img>
+              </Link>
             </div>
           </div>
-          <div className="hl-games-list-item">
-            <div className="img-bg">
-              <img className="homeImages" src="https://cdn.imgbin.com/24/24/2/imgbin-league-of-legends-electronic-sports-legacy-esports-pty-limited-player-league-of-legends-e6g009zLZUBPBWg8B5uAigahK.jpg" alt="Game"></img>
-            </div>
-          </div>
-          <div className="hl-games-list-item">
-            <div className="img-bg">
-              <img className="homeImages" src="https://www.pngfind.com/pngs/m/503-5039968_lgd-png-download-player-transparent-png.png" alt="Game"></img>
-            </div>
-          </div>
-          <div className="hl-games-list-item">
-            <div className="img-bg">
-              <img className="homeImages" src="https://images.squarespace-cdn.com/content/v1/589bff0c414fb513e704c6e9/1633339929071-Y6VF294WJH8L5NP56W5M/IMG_7789.jpeg" alt="Game"></img>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+        :
+        <></>
+      }
     </div>
   )
 }
